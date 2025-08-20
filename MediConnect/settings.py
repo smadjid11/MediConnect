e2b0bc5beaf772a0623c7f075b1e494dc96d94af
Seghier Abdel Madjid <seghierabdelmadjid11@gmail.com>
2025-08-20 12:51:32 -0700

    Update settings.py

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'MediConnect\static')
+    os.path.join(BASE_DIR, 'MediConnect/static')
 ]
 
 MEDIA_URL = 'media/'
